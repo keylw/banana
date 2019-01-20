2fb91b51b1baa2a501c991b84daacbf00cfd7f72
Keylw <keylung94@gmail.com>
2019-01-20 17:49:39 +0900

    added std as a feature

--- banan.py
+++ banan.py
@@ -21,15 +21,25 @@ class NN():
 
     @staticmethod
     def euclidean_distance(x,y):
-        print(x,y)
-        return np.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2) 
+        # print(x,y)    
+        R_m = (x[0] - y[0])**2
+        G_m = (x[0] - y[0])**2
+        B_m = (x[0] - y[0])**2
+
+        R_s = (x[0] - y[0])**2
+        G_s = (x[0] - y[0])**2
+        B_s = (x[0] - y[0])**2
+
+
+        return np.sqrt(R_m + G_m + B_m + R_s + G_s + B_s) 
 
     @staticmethod
     def train(datafile = "training_banana/banana.csv"):
         with open(datafile, 'r') as myfile:
             csv_reader = csv.reader(myfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
             for row in csv_reader:
-                d = (row[3],[row[0],row[1],row[2]])
+                print(row)
+                d = (row[6],[row[0],row[1],row[2],row[3],row[4],row[5]])
                 NN.data.append(d)
         
 
@@ -48,40 +58,63 @@ def extract_banana(image):
 
     return banana, image
 
-def extract_rgb(image, banana):
+def extract_rgb_features(image, banana):
     mask = banana_mask(image, banana)
     area = cv2.contourArea(banana)
-    mean_val = cv2.mean(image, mask = mask)
+    mean, std = cv2.meanStdDev(image, mask = mask)
 
-    print(mean_val)
-    B = mean_val[0]
-    G = mean_val[1]
-    R = mean_val[2]
-    return [R,G,B]
+    B_mean = mean[0][0]
+    G_mean = mean[1][0]
+    R_mean = mean[2][0]
+
+    B_std = std[0][0]
+    G_std = std[1][0]
+    R_std = std[2][0]
+
+    print(R_mean)
+    return [R_mean,G_mean,B_mean,R_std,G_std,B_std]
 
 def predict(image):
     b, _img = extract_banana(image)
-    d = extract_rgb(image, b)
-    print(NN.predict(d))
-
-    cv2.imshow('image', _img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    d = extract_rgb_features(image, b)
+    p = NN.predict(d)
+    show_result(_img, p, d)
 
 
 def train_NN():
     data = []
-    for i in range(1,7):
+    for i in range(1,8):
         im = cv2.imread('training_banana/b{}.png'.format(i))
         b, _img = extract_banana(im)
-        d = extract_rgb(im, b)
+        d = extract_rgb_features(im, b)
         d.append(i)
-        data.append(d)
-
+        data.append(d)    
     with open("training_banana/banana.csv", 'w', newline='') as myfile:
         wr = csv.writer(myfile)
         for d in data: 
             wr.writerow(d)
+
+def show_result(image, pred, rgb_values):
+    import matplotlib.pyplot as plt
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    res = [0,0,0,0,0,0,0]
+    res[int(pred)-1] += 1
+
+    label = ('1', '2', '3', '4', '5', '6', '7')
+    y_pos = np.arange(len(label))
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.imshow(image)
+
+    plt.subplot(212)
+    plt.bar(y_pos, res, align='center', alpha=0.5)
+    plt.xticks(y_pos, label)
+    plt.ylabel('Ripness level')
+    plt.title('Banana')
+    plt.show()
+
 
 def main():
     NN.train()
@@ -92,7 +125,8 @@ def main():
     image = cv2.imread(img)
     predict(image)
 
+
 if __name__ == "__main__":
-    # train_NN()
+    train_NN()
     main()
 
